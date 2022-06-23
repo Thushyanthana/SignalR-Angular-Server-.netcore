bcc370b7f3f1b40f7fd822043f1496f6e08c2478
unknown <thushiravirajan01@gmail.com>
2022-06-23 07:43:32 +0530

    Initial server-sinalR-.netCore

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,33 @@
+﻿using Chat_Server.Hubs;
+using Chat_Server.ReqDto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat_Server.Controllers
+{
+    [Route("api/chat")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub> _hubContext;
+
+        public ChatController(IHubContext<ChatHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [Route("send")]
+        [HttpPost]
+        public IActionResult SendRequest([FromBody] MessageDto msg)
+        {
+            _hubContext.Clients.All.SendAsync("ReceiveOne", msg.user, msg.msgText);
+            return Ok(msg);
+        }
+
+    }
+}
---
+++ ReqDto/MessageDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat_Server.ReqDto
+{
+    public class MessageDto
+    {
+
+        public string user { get; set; }
+        public string msgText { get; set; }
+    }
+}
